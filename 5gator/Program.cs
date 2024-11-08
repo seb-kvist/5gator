4ed03b20d7d622b14f1597b0b2f47e3e1b5f4ba2
Sebastian Kvist <sebastian_kvist@hotmail.com>
2024-11-08 20:23:49 +0100

    ovning

---
+++ 5gator/Program.cs
@@ -0,0 +1,28 @@
+﻿int yourPoints = 0;
+
+do {
+    //RESTORE POINTS IF FAILURE
+    yourPoints = 0; 
+    Console.WriteLine("I det här spelet kommer du få svara på fem gåtor. Du får 3 chanser att svara på varje gåta. ");
+    Console.WriteLine("Tryck på Enter för att börja...");
+    Console.ReadLine();  // Väntar på att användaren trycker Enter
+
+    //GÅTA NR 1
+    System.Console.WriteLine("GÅTA 1");
+    System.Console.WriteLine("Jag kan inte ses, men jag finns överallt. Jag är ofta blå, men ibland grå. Vad är jag?");
+    int numberofGuesses = 3;
+    while (numberofGuesses > 0)
+    {
+      var guess = Console.ReadLine().Trim().ToLower();  
+      if (guess == "himlen")
+        {
+            System.Console.WriteLine("Rätt svar!");
+            yourPoints++;
+            break;
+        } else {
+            numberofGuesses--;
+            System.Console.WriteLine("Fel svar dumbom, försök igen!");
+            System.Console.WriteLine($);
+        }
+    }
+}
